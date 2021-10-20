7f5ec1a43c7290e3e3f067d30cfbb5e0878379d5
Artur Puzio <apuzio@studentcoin.org>
2021-10-20 11:54:15 +0200

    Burn tests

--- test/LEANERC20.test.js
+++ test/LEANERC20.test.js
@@ -1,12 +1,12 @@
 // test/LEANERC20.sol
 // Load dependencies
-const { expect } = require('chai')
-const BN = require('bn.js');
-const LEAN = artifacts.require('LEANERC20')
+const { expect } = require('chai');
+const { BN, ether, expectRevert } = require('@openzeppelin/test-helpers');
+const LEAN = artifacts.require('LEANERC20');
 
 const hextab = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 const mkAddr = (x) => "0xf0000000000000000000000000000000000000" + hextab[x/10 | 0] + hextab[x%10]
-const totalSupply = new BN("3000000000000000000000000") // 10 mln 
+const totalSupply = ether("3000000") // 3 mln 
 
 contract('LEAN', accounts => {
   beforeEach(async function () {
@@ -36,6 +36,23 @@ contract('LEAN', accounts => {
         expect((await this.token.balanceOf(accounts[0])).toString()).to.equal('0')
         for (let i = 0; i < n; i++)
             expect((await this.token.balanceOf(mkAddr(i))).toString()).to.equal(v.toString())
+    }
+    this.doFirstBurnDeployer = async function(amount) {
+      expect((await this.token.totalSupply()).toString()).to.equal(totalSupply.toString());
+      expect((await this.token.balanceOf(accounts[0])).toString()).to.equal(totalSupply.toString());
+      const {receipt: receipt} = await this.token.burn(amount);
+      console.log("Burn used " + receipt.cumulativeGasUsed + " gas");
+      expect((await this.token.totalSupply()).toString()).to.equal(totalSupply.sub(amount).toString());
+      expect((await this.token.balanceOf(accounts[0])).toString()).to.equal(totalSupply.sub(amount).toString());
+    }
+
+    this.doAnyBurn = async function(account, amount) {
+      const beforeSupply = await this.token.totalSupply();
+      const beforeBalance = await this.token.balanceOf(account);
+      const {receipt: receipt} = await this.token.burn(amount, { from: account });
+      console.log("Burn used " + receipt.cumulativeGasUsed + " gas");
+      expect((await this.token.totalSupply()).toString()).to.equal(beforeSupply.sub(amount).toString());
+      expect((await this.token.balanceOf(account)).toString()).to.equal(beforeBalance.sub(amount).toString());
     }
   });
 
@@ -72,4 +89,42 @@ contract('LEAN', accounts => {
     await this.doBatchTransfers(100)
   });
 
+  it('Can burn small', async function () {
+    await this.doFirstBurnDeployer(ether("1"));
+    await this.doAnyBurn(accounts[0], ether("1"));
+    await this.doAnyBurn(accounts[0], ether("100"));
+  });
+
+  it('Can burn big', async function () {
+    await this.doFirstBurnDeployer(ether("1000000"));
+    await this.doAnyBurn(accounts[0], ether("30000"));
+    await this.doAnyBurn(accounts[0], ether("500000"));
+  });
+
+  it('Can burn all', async function () {
+    await this.doFirstBurnDeployer(ether("3000000"));
+    expect((await this.token.balanceOf(accounts[0])).toString()).to.equal("0");
+    expect((await this.token.totalSupply()).toString()).to.equal("0");
+  });
+
+  describe('Burn by non deployer', function() {
+    beforeEach(async function () {
+      await this.token.transfer(accounts[1], ether("100"));
+      expect((await this.token.balanceOf(accounts[0])).toString()).to.equal(ether("2999900").toString());
+      expect((await this.token.balanceOf(accounts[1])).toString()).to.equal(ether("100").toString());
+    });
+    it('Transfer & burn', async function () {
+      await this.doAnyBurn(accounts[1], ether("20"));
+      expect((await this.token.balanceOf(accounts[0])).toString()).to.equal(ether("2999900").toString());
+      expect((await this.token.balanceOf(accounts[1])).toString()).to.equal(ether("80").toString());
+    });
+  
+    it('Transfer & fail burn', async function () {
+      await expectRevert(this.token.burn(ether("101"), { from: accounts[1] }),
+        'ERC20: burn amount exceeds balance'  
+      );
+      expect((await this.token.balanceOf(accounts[0])).toString()).to.equal(ether("2999900").toString());
+      expect((await this.token.balanceOf(accounts[1])).toString()).to.equal(ether("100").toString());
+    });
+  });
 });
